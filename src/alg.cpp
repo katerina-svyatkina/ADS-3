23f6da8e39842190ab86258e6e4219b10cc61c10
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-02-27 15:18:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+   int right=size-1, left = 0, k=0;
+   int half_size = (right + left) / 2;
+   if(size == 0) {
+       return 0;
+   }
+   if (arr[half_size] == value) {
+       k++;
+   }
+   else if (arr[half_size] > value) {
+       right = half_size;
+   }
+   else {
+       left = half_size;
+   }
+       return k + cbinsearch(arr, half_size-left, value) + cbinsearch(&(arr[half_size+1]), right-half_size, value);
 }
